a26d27857b7d017358243c7a758043ece7943f65
silvercrowkws <silvercrowkws@naver.com>
2024-08-07 15:11:47 +0900

    사이클롭스 몬스터 특성 완성
    10% 확률로 충돌시 유닛 제거

--- Assets/Scripts/Enemy/Monster/CyclopsMonster.cs
+++ Assets/Scripts/Enemy/Monster/CyclopsMonster.cs
@@ -7,6 +7,11 @@ public class CyclopsMonster : MonsterBase
     Player player;
     GameManager gameManager;
 
+    /// <summary>
+    /// 유닛을 파괴할 때 보일 레이저 루트
+    /// </summary>
+    GameObject laserRoot;
+
     protected override void Start()
     {
         moveSpeed = 2.5f;
@@ -15,6 +20,10 @@ public class CyclopsMonster : MonsterBase
 
         gameManager = GameManager.Instance;
         player = GameManager.Instance.Player;
+
+        Transform child = transform.GetChild(0);
+        laserRoot = child.gameObject;
+        laserRoot.SetActive(false);
 
         base.Start();
 
@@ -39,9 +48,35 @@ public class CyclopsMonster : MonsterBase
                 player.soliderObjectDictionary.Remove(DestroyCellPosition);     // Dictionary에서 제거
                 player.objectSoliderDictionary.Remove(collisionGameObject);     // Dictionary에서 제거
 
+                StartCoroutine(LaserCoroutine(collisionGameObject));
+
                 Debug.Log("10% 유닛 파괴");
             }
         }
+    }
+
+    /// <summary>
+    /// 충돌시 10% 확률로 유닛을 제거하는 코루틴
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    IEnumerator LaserCoroutine(GameObject target)
+    {
+        laserRoot.transform.position = this.gameObject.transform.position;      // 위치 맞추고
+        laserRoot.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);    // 각도 초기화 하고
+        laserRoot.gameObject.SetActive(true);                                   // 게임 오브젝트 활성화
+
+        Transform laserTransform = laserRoot.gameObject.transform;              // laserRoot의 Transform 가져오기
+
+        // 타겟과 laserRoot 사이의 방향 벡터 계산
+        Vector3 direction = target.transform.position - laserTransform.position;
+
+        // 방향 벡터의 각도를 계산하여 laserRoot의 rotation을 설정
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        laserTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90));      // Atan2로 얻은 각도는 유니티의 기본 방향과 맞지 않아
+                                                                                        // 90도를 더해줌
+        yield return new WaitForSeconds(0.2f);                                          // 0.2초 후
+        laserRoot.gameObject.SetActive(false);                                          // 게임 오브젝트 비활성화
     }
 
 
